82e74773d2f8f4dfaff519fce6c1d3c9016424cb
aadesh0928 <46253413+aadesh0928@users.noreply.github.com>
2021-07-15 14:53:07 +0530

    Add files via upload

---
+++ Nedbank.HR.SAP.Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Nedbank.HR.SAP.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string EmployeeNumber { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeSurname { get; set; }
+        public string EmployeeEmailAddress { get; set; }
+        public string EmployeeCellularNumber { get; set; }
+        public string EmployeeOfficeTelephoneNumber { get; set; }
+        public string EmployeeBranchID { get; set; }
+        public string EmployeeBranchNumber { get; set; }
+        public string EmployeeBranchName { get; set; }
+        public string EmployeePhysicalLocationFloor { get; set; }
+        public string EmployeePhysicalLocationBuildingName { get; set; }
+        public string EmployeePhysicalLocationStreetNumber { get; set; }
+        public string EmployeePhysicalLocationAddress { get; set; }
+        public string EmployeePhysicalLocationSuburb { get; set; }
+        public string EmployeePhysicalLocationCity { get; set; }
+        public string EmployeePhysicalLocationProvince { get; set; }
+        public string EmployeePhysicalLocationCountry { get; set; }
+        public string EmployeeNickName { get; set; }
+        public string EmployeeJobTitle { get; set; }
+        public string EmployeeCluster { get; set; }
+
+    }
+}
